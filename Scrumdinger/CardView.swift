c9dd689ba0461b69ef9784186aab85a40b93652f
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-07-26 21:17:35 +0300

    Section 2.3

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,37 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Bohdan Pohorilets on 26.07.2022.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20.0)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
