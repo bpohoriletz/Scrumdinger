5575174bdcfa0e4bd0add23d2e3f10f6f5db1131
Bohdan Pohorileta <bohdan.pohorilets@gmail.com>
2022-07-26 19:03:54 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Bohdan Pohorilets on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
