d8fc89911c16b735ec0ab499f39970a3a570cf18
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-07-28 18:21:27 +0300

    Section 4.1

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ScrumView(scrums: DailyScrum.sampleData)
+            NavigationView{
+                ScrumView(scrums: DailyScrum.sampleData)
+            }
         }
     }
 }
