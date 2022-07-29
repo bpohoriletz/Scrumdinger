bdd4bcbcc4b187fa4bbf3a622d9a3a00986305c1
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-07-29 20:09:10 +0300

    Section 10.2

---
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -0,0 +1,56 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Bohdan Pohorilets on 29.07.2022.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    
+    var body: some View {
+        VStack {
+            ProgressView(value: progress)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed )", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Time Remaining")
+        .accessibilityValue("\(minutesRemaining) minutes")
+    }
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        
+        return Double(secondsElapsed) / Double(totalSeconds)
+    }
+    
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView(secondsElapsed: 300, secondsRemaining: 600)
+            .previewLayout(.sizeThatFits)
+    }
+}
