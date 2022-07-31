510a245ac03f74244f2303c0513bbb83077035d5
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-07-31 21:47:14 +0300

    Section 12.5

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-    @State private var scrums = DailyScrum.sampleData
+    @StateObject private var store = ScrumStore()
     
     var body: some Scene {
         WindowGroup {
             NavigationView{
-                ScrumView(scrums: $scrums)
+                ScrumView(scrums: $store.scrums)
+            }.onAppear(){
+                ScrumStore.load{ result in
+                    switch result {
+                    case .failure(let error):
+                        fatalError(error.localizedDescription)
+                    case .success(let scrums):
+                        store.scrums = scrums
+                    }
+                }
             }
         }
     }
