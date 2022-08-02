99893106af93f29c79ffc116f5c8eabc022fd9eb
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-08-02 19:57:50 +0300

    Section 16.2

---
+++ Scrumdinger/Views/ErrorView.swift
@@ -0,0 +1,44 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by Bohdan Pohorilets on 02.08.2022.
+//
+
+import SwiftUI
+
+struct ErrorViw: View {
+    let errorWrapper: ErrorWrapper
+    
+    var body: some View {
+        VStack {
+            Text("An error has occurred!")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
+    }
+}
+
+struct ErrorViw_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired,
+                     guidance: "You can safely ignore this error.")
+    }
+    
+    static var previews: some View {
+        ErrorViw(errorWrapper: wrapper)
+    }
+}
