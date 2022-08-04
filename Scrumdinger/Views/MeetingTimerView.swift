fb97edc8625210fe52ac58ae732681332a72b4ee
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2022-08-04 12:37:03 +0300

    Section 17.1

---
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -0,0 +1,40 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by Bohdan Pohorilets on 04.08.2022.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    var theme: Theme
+    var speakers: [ScrumTimer.Speaker]
+    private var currentSpeaker: String {
+        speakers.first(where: { !$0.isCompleted })?.name ?? "Someone"
+    }
+    
+    var body: some View {
+        Circle()
+            .strokeBorder(lineWidth: 24)
+            .overlay {
+                VStack {
+                    Text(currentSpeaker)
+                        .font(.title)
+                    Text("is speaking")
+                }
+                .accessibilityElement(children: .combine)
+                .foregroundStyle(theme.accentColor)
+            }
+    }
+}
+
+struct MeetingTimerView_Previews: PreviewProvider {
+    static var speakers: [ScrumTimer.Speaker] {
+        [ScrumTimer.Speaker(name: "Bill", isCompleted: true), ScrumTimer.Speaker(name: "Cathy", isCompleted: false)]
+    }
+    
+    static var previews: some View {
+        MeetingTimerView(theme: .yellow, speakers: speakers)
+    }
+}
